7596caef6a426e67d42121693d99553dabdd3809
wangbo <781278657@qq.com>
2022-04-20 13:35:11 +0800

    add：typescript 泛型参数默认值

--- typescript泛型参数默认值.ts
+++ typescript泛型参数默认值.ts
@@ -14,36 +14,52 @@
 // 考虑一个会创建新的 HTMLElement 的函数，调用时不加参数会生成一个Div，你也可以选择性地传入子元素的列表。
 // 之前你必须这么去定义：
 
-type Container<T, U> = {
-  element: T,
-  children: U
-}
-
-function create(): Container<HTMLDivElement, HTMLDivElement[]>;
-function create<T extends HTMLElement>(element: T): Container<T, T[]>;
-function create<T extends HTMLElement, U extends HTMLElement>(
-  element: T,
-  children: U[]
-): Container<T, U[]>;
-function create<T extends HTMLElement, U extends T[]>(element?: T, children?: U): Container<T, U> {
-  return {
-    element: element as T,
-    children: children as U
-  }
-}
-
-// 有了泛型参数默认类型，我们可以将定义化简为：
-// function create<T extends HTMLElement = HTMLDivElement, U = T[]>(
-//   element?: T,
-//   children?: U
-// ): Container<T, U> {
+// type Container<T, U> = {
+//   element: T,
+//   children: U
+// }
+//
+// function create(): Container<HTMLDivElement, HTMLDivElement[]>;
+// function create<T extends HTMLElement>(element: T): Container<T, T[]>;
+// function create<T extends HTMLElement, U extends HTMLElement>(
+//   element: T,
+//   children: U[]
+// ): Container<T, U[]>;
+// function create<T extends HTMLElement, U extends T[]>(element?: T, children?: U): Container<T, U> {
 //   return {
 //     element: element as T,
 //     children: children as U
 //   }
 // }
 
-const r1 = create()
-const r2 = create(document.createElement('dialog'))
-const r3 = create(document.createElement('dialog'), [document.createElement('div')])
+
+type Container<T, U> = {
+  element: T,
+  children: U
+}
+
+function create<T extends HTMLElement = HTMLDivElement, U = T[]>(element?: T, children?: U): Container<T, U> {
+  return {
+    element: element as T,
+    children: children as U
+  }
+}
+
+const r1 = create() // Container<HTMLDivElement, HTMLDivElement[]>
+const r2 = create(document.createElement('dialog')) // Container<HTMLDialogElement, HTMLDialogElement[]>
+const children = [document.createElement('div')] // HTMLDivElement[]
+const r3 = create(document.createElement('dialog'), children) // Container<HTMLDialogElement, HTMLDivElement[]>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
